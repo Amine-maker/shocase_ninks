c8cdfff53e8dedef1e3b6f58b99e7e4d7a29404d
Amine DJENNADI <amine.djennadi@ocapiat.fr>
2023-09-14 11:33:28 +0200

    add lang

---
+++ src/js/lang.js
@@ -0,0 +1,138 @@
+const lang = new Map()
+
+lang.set("fr", {
+  header: {
+    title: "",
+    subtitleText: "",
+    adsLargeText: "",
+    button: "",
+  },
+
+  slider: [
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+  ],
+  pricing: [
+    { price: "", functionnality: ["", "", ""], button: "" },
+    { price: "", functionnality: ["", "", ""], button: "" },
+  ],
+  finalScreen: {
+    title: "",
+    subtitleText: "",
+    button: "",
+  },
+  footer: {
+    legal: "",
+    confidentiality: "",
+    right: "",
+  },
+})
+
+lang.set("eng", {
+  header: {
+    title: "",
+    subtitleText: "",
+    adsLargeText: "",
+    button: "",
+  },
+
+  slider: [
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+  ],
+  pricing: [
+    { price: "", functionnality: ["", "", ""], button: "" },
+    { price: "", functionnality: ["", "", ""], button: "" },
+  ],
+  finalScreen: {
+    title: "",
+    subtitleText: "",
+    button: "",
+  },
+  footer: {
+    legal: "",
+    confidentiality: "",
+    right: "",
+  },
+})
+lang.set("es", {
+  header: {
+    title: "",
+    subtitleText: "",
+    adsLargeText: "",
+    button: "",
+  },
+
+  slider: [
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+    {
+      title: "",
+      description: "",
+    },
+  ],
+  pricing: [
+    { price: "", functionnality: ["", "", ""], button: "" },
+    { price: "", functionnality: ["", "", ""], button: "" },
+  ],
+  finalScreen: {
+    title: "",
+    subtitleText: "",
+    button: "",
+  },
+  footer: {
+    legal: "",
+    confidentiality: "",
+    right: "",
+  },
+})
